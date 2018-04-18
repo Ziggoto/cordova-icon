2be7507d874f35e8901f493c1cb524bb9b29abbb
Fábio Theophilo <ftheophilo@greenmile.com.br>
2018-04-18 16:52:03 -0300

    Makes destination file to has same name of input file

--- index.js
+++ index.js
@@ -71,19 +71,19 @@ var getPlatforms = function (projectName) {
     isAdded : fs.existsSync('platforms/android'),
     iconsPath : 'src/static/android/',
     icons : [
-      { name : 'drawable/icon.png',       size : 96 },
-      { name : 'drawable-hdpi/icon.png',  size : 72 },
-      { name : 'drawable-ldpi/icon.png',  size : 36 },
-      { name : 'drawable-mdpi/icon.png',  size : 48 },
-      { name : 'drawable-xhdpi/icon.png', size : 96 },
-      { name : 'drawable-xxhdpi/icon.png', size : 144 },
-      { name : 'drawable-xxxhdpi/icon.png', size : 192 },
-      { name : 'mipmap-hdpi/icon.png',  size : 72 },
-      { name : 'mipmap-ldpi/icon.png',  size : 36 },
-      { name : 'mipmap-mdpi/icon.png',  size : 48 },
-      { name : 'mipmap-xhdpi/icon.png', size : 96 },
-      { name : 'mipmap-xxhdpi/icon.png', size : 144 },
-      { name : 'mipmap-xxxhdpi/icon.png', size : 192 }
+      { name : 'drawable/' + settings.ICON_FILE,       size : 96 },
+      { name : 'drawable-hdpi/' + settings.ICON_FILE,  size : 72 },
+      { name : 'drawable-ldpi/' + settings.ICON_FILE,  size : 36 },
+      { name : 'drawable-mdpi/' + settings.ICON_FILE,  size : 48 },
+      { name : 'drawable-xhdpi/' + settings.ICON_FILE, size : 96 },
+      { name : 'drawable-xxhdpi/' + settings.ICON_FILE, size : 144 },
+      { name : 'drawable-xxxhdpi/' + settings.ICON_FILE, size : 192 },
+      { name : 'mipmap-hdpi/' + settings.ICON_FILE,  size : 72 },
+      { name : 'mipmap-ldpi/' + settings.ICON_FILE,  size : 36 },
+      { name : 'mipmap-mdpi/' + settings.ICON_FILE,  size : 48 },
+      { name : 'mipmap-xhdpi/' + settings.ICON_FILE, size : 96 },
+      { name : 'mipmap-xxhdpi/' + settings.ICON_FILE, size : 144 },
+      { name : 'mipmap-xxxhdpi/' + settings.ICON_FILE, size : 192 }
     ]
   });
   platforms.push({
